01c506c774d3f3e749d086fdcd0098434d3d70b8
Dane Burkland <dane.a.burkland@gmail.com>
2015-03-19 17:43:38 -0500

    added GE to nav

--- js/main.js
+++ js/main.js
@@ -1,124 +1,19 @@
-var pathObj = {
-    "svgtest": {
-        "strokepath": [
-            {
-                "path": "M29.7215212,178.945949 C29.7213729,185.552968 19.7215208,185.788941 19.7215212,179.063932 C19.7215216,172.338923 29.7216695,172.33893 29.7215212,178.945949 Z",
-                "duration": 450
-            },
-            {
-                "path": "M29.4861006,179.166667 C29.4861006,179.166667 29.2025864,179.166667 55.7662307,179.166667 C82.329875,179.166667 84.1520785,115.888655 84.1520756,101.37535 C84.1520727,86.8620448 85.9742762,41.0000022 134.080333,41 L157.042934,41",
-                "duration": 450
-            },
-            {
-                "path": "M194.146484,41.0527344 L200.664074,41.0527344 L203.146484,29.03125 L205.683594,44.0722656 L208.183594,38.0722656 L210.683594,42.0722656 L213.646484,33.5722656 L216.646484,51.5722656 L219.109375,39.5722656 L230.109375,39.5722656",
-                "duration": 450
-            },
-            {
-                "path": "M29.7215212,179.166667 L157.278479,179.166667",
-                "duration": 450
-            },
-            {
-                "path": "M190.146484,180.917643 L196.664074,180.917643 L199.146484,168.896159 L201.683594,183.937174 L204.183594,177.937174 L206.683594,181.937174 L209.646484,173.437174 L212.646484,191.437174 L215.109375,179.437174 L226.109375,179.437174",
-                "duration": 450
-            },
-            {
-                "path": "M29.4861006,178.833333 C29.4861006,178.833333 29.2025864,178.833333 55.7662307,178.833333 C82.329875,178.833333 84.1520785,242.111345 84.1520756,256.62465 C84.1520727,271.137955 85.9742762,316.999998 134.080333,317 L157.042934,317",
-                "duration": 450
-            },
-            {
-                "path": "M199.96745,312.57343 C199.96738,317.449822 195.253372,317.623984 195.253372,312.660508 C195.253372,307.697032 199.96752,307.697038 199.96745,312.57343 Z",
-                "duration": 100
-            },
-            {
-                "path": "M197.733577,316.543966 C197.733577,316.543966 197.733577,317.117991 197.733577,323.991 C197.733577,323.991 197.032354,328 204.001799,328 C210.971244,328 211.170371,326.273391 211.170371,324.272687 C211.170371,322.271983 211.269934,316.628618 211.269934,316.628618 C211.269934,316.628618 211.322309,316.628618 211.348496,316.628618 C213.249666,316.628618 217.052007,316.628618 217.052007,316.628618 C217.052007,316.628618 218.571906,316.590954 218.571906,314.20428 C218.571906,311.817606 218.571906,306.700055 218.571906,306.700055 C218.571906,306.700055 218.855817,305.458984 217.052006,305.458984 C215.248195,305.458984 206.180752,305.458984 206.180752,305.458984 C206.180752,305.458984 204.794029,305.458984 204.794029,306.700055 C204.794029,307.941125 204.794029,314.7311 204.794029,314.7311 C204.794029,314.7311 204.886426,316.628617 206.180752,316.628618 C207.475077,316.628618 211.348496,316.628618 211.348496,316.628618",
-                "duration": 250
-            },
-            {
-                "path": "M215.498004,311.232199 C215.49789,316.108591 207.800674,316.282754 207.800674,311.319278 C207.800674,306.355801 215.498118,306.355807 215.498004,311.232199 Z",
-                "duration": 100
-            },
-            {
-                "path": "M211.006455,311.114796 L212.639167,309.405093 L211.849145,311.416508 C211.849145,311.416508 211.638472,311.818791 211.217127,311.818791 C210.795782,311.818791 211.006455,311.114796 211.006455,311.114796 Z",
-                "duration": 100
-            },
-            {
-                "path": "M190.246927,17 L182,17 L182,65.465359 L190.61253,65.465359",
-                "duration": 5
-            },
-            {
-                "path": "M334.365603,17 L342.61253,17 L342.61253,65.465359 L334,65.465359",
-                "duration": 5
-            },
-            {
-                "path": "M190.246927,156 L182,156 L182,204.465359 L190.61253,204.465359",
-                "duration": 5
-            },
-            {
-                "path": "M334.365603,156 L342.61253,156 L342.61253,204.465359 L334,204.465359",
-                "duration": 5
-            },
-            {
-                "path": "M190.246927,293 L182,293 L182,341.465359 L190.61253,341.465359",
-                "duration": 5
-            },
-            {
-                "path": "M334.365603,293 L342.61253,293 L342.61253,341.465359 L334,341.465359",
-                "duration": 5
-            }
-        ],
-        "dimensions": {
-            "width": 386,
-            "height": 360
-        }
-    }
-}; 
+/*Prepare the strengthen indicators*/
+var checkmarkPath = document.querySelector('#checkmark');
+var checkmarkLength = checkmarkPath.getTotalLength();
+var stepProgressPath = document.querySelector('#step-progress');
+var stepProgressLength = stepProgressPath.getTotalLength();
+var sleepProgressPath = document.querySelector('#sleep-progress');
+var sleepProgressLength = sleepProgressPath.getTotalLength();
 
-var pathObjTwo = {
-    "svgstrengthen": {
-        "strokepath": [
-/*            {
-                "path": "M24.9765625,-0.545559645 L24.9765625,328.636353",
-                "duration": 600
-            },*/
-            {
-                "path": "M29.9765625,334.128117 C29.9764142,340.735136 19.9765621,340.971109 19.9765625,334.2461 C19.9765629,327.52109 29.9767108,327.521098 29.9765625,334.128117 Z",
-                "duration": 600
-            },
-/*            {
-                "path": "M25.0034009,340.456022 L25.0034009,479.48185",
-                "duration": 600
-            },*/
-            {
-                "path": "M29.9765625,484.422467 C29.9764142,491.029486 19.9765621,491.26546 19.9765625,484.54045 C19.9765629,477.815441 29.9767108,477.815448 29.9765625,484.422467 Z",
-                "duration": 600
-            },
-        ],
-        "dimensions": {
-            "width": 153,
-            "height": 593
-        }
-    }
-}; 
+document.getElementById('checkmark').style.strokeDasharray = checkmarkLength;
+document.getElementById('checkmark').style.strokeDashoffset = checkmarkLength;
+document.getElementById('step-progress').style.strokeDasharray = stepProgressLength;
+document.getElementById('step-progress').style.strokeDashoffset = -stepProgressLength;
+document.getElementById('sleep-progress').style.strokeDasharray = sleepProgressLength;
+document.getElementById('sleep-progress').style.strokeDashoffset = -sleepProgressLength;
 
 $(document).ready(function(){
-  var $featuresvg = $('#svgtest').lazylinepainter( 
-    {
-      "svgData": pathObj,
-      "strokeWidth": 1,
-      "strokeColor": "#FAB216",
-    });
-
-   var $svgstrengthen = $('#svgstrengthen').lazylinepainter( 
-    {
-      "svgData": pathObjTwo,
-      "strokeWidth": 1,
-      "strokeColor": "#FAB216"
-    }); 
-
-/*   var $navText = $(".onepage-pagination li a p");
-   var $navBar = $(".onepage-pagination");*/
-
-
   $(".main").onepage_scroll({
      sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
      easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
@@ -138,6 +33,15 @@ $(document).ready(function(){
         $(".onepage-pagination").addClass('nav-fill-white')
         $("#pulseband-top").addClass('pulseband-feature')
         $("#pulseband-top").removeClass('pulseband-jumbotron-fade')
+        $("#pulseband-top").removeClass('pulseband-strengthen')
+      }
+      if(index==3) {
+        $("#pulseband-top").addClass('pulseband-strengthen')
+        $("#pulseband-top").removeClass('pulseband-jumbotron-fade')
+      }
+      if(index==4) {
+        $("#pulseband-top").removeClass('pulseband-strengthen')
+        $("#pulseband-top").addClass('pulseband-jumbotron-fade')
       }
 
      },  // This option accepts a callback function. The function will be called before the page moves.
@@ -150,25 +54,23 @@ $(document).ready(function(){
       }
       if(index==2) {
         $("#line-drawing-jumbotron").css('opacity', '1')
-        $featuresvg.lazylinepainter('paint')
         $(".indicators").animate({
           "top": "42%",
           "left": "50%",
           "width": "15%",
           "height": "50%"
         })
-
       };
       if(index==3) {
-        $svgstrengthen.lazylinepainter('paint')
         $("#fact-one").addClass('animated fadeInRight')
         $("#fact-two").addClass('animated fadeInRight')
+        $("#step-progress").attr('class', 'step-progress step-progress-draw' )
+        $("#sleep-progress").attr('class', 'sleep-progress sleep-progress-draw' )
+        $("#checkmark").attr('class', 'checkmark checkmark-draw' )
       };
       if(index==4) {
         $("#off").addClass('animated fadeInRightShort')
         $("#on").addClass('animated fadeIn')
-      }
-      if(index==5) {
       }
      },   // This option accepts a callback function. The function will be called after the page moves.
      loop: false,
@@ -181,7 +83,7 @@ $(document).ready(function(){
   });
   
   $(".onepage-pagination").addClass('hidden')
-  $(".onepage-pagination li:nth-child(1) a").append("<p>Pulseband</p>");
+  $(".onepage-pagination li:nth-child(1) a").append("<p>Pulseband<br><span>by General Electric</span></p>");
   $(".onepage-pagination li:nth-child(2) a").append("<p>Highlights</p>");
   $(".onepage-pagination li:nth-child(3) a").append("<p>Features</p>");
   $(".onepage-pagination li:nth-child(4) a").append("<p>App</p>");
@@ -191,8 +93,6 @@ $(document).ready(function(){
 
 });
 
-$(document).unbind('mousewheel DOMMouseScroll');
-
 
 /***************** Flickity ******************/
 
@@ -200,6 +100,7 @@ $(document).unbind('mousewheel DOMMouseScroll');
     cellAlign: 'left',
     contain: true,
     prevNextButtons: false,
+    autoPlay: true,
     imagesLoaded: true
   });
 
